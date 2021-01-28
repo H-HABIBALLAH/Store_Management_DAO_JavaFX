15042d03c24fae62de1b2ef9e82bedbc9b09faf6
HABIBALLAH <hamza1habiballah@gmail.com>
2021-01-28 21:50:03 +0100

    Queries functions - product part

--- MainToTest.java
+++ MainToTest.java
@@ -2,14 +2,13 @@ package StoreManagement;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.List;
 
 public class MainToTest {
     public static void main(String[] args) {
         ProduitDaoImpl produitDao=new ProduitDaoImpl();
-        Calendar date=new Calendar();
-        Produit produit=new Produit(4,"Galaxy s8",5,4000,date.toLocalDate());
+        Date date=new Date(2020,1,28);
+        Produit produit=new Produit(3,"Galaxy s8",5,4000,date.toLocalDate());
         produitDao.add(produit);
     }
 }
--- ProduitDaoImpl.java
+++ ProduitDaoImpl.java
@@ -1,5 +1,6 @@
 package StoreManagement;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,13 +11,16 @@ public class ProduitDaoImpl extends AbstractDao implements IProduitDao{
 
     @Override
     public void add(Produit obj) {
-        List<Produit> list = new ArrayList<Produit>();
         PreparedStatement pst=null;
-        String sql = "INSERT INTO `produit`(`id`, `designation`, `qte`, `prix`, `date`) VALUES (["+obj.getId()+"],["+obj.getDesignation()+"],["+obj.getQuantity()+"],["+obj.getPrix()+"],["+obj.getDate()+"])";
+        String sql = "INSERT INTO produit (designation, qte, prix, date) VALUES (?,?,?,?)";
         try {
             pst=connection.prepareStatement(sql);
+            pst.setString(1,obj.getDesignation());
+            pst.setInt(2,obj.getQuantity());
+            pst.setDouble(3,obj.getPrix());
+            pst.setDate(4, Date.valueOf(obj.getDate()));
+            pst.executeUpdate();
             System.out.println("Success d'exec requete");
-            ResultSet rs=pst.executeQuery();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -24,23 +28,34 @@ public class ProduitDaoImpl extends AbstractDao implements IProduitDao{
 
     @Override
     public void delete(long id) {
-
+        PreparedStatement pst=null;
+        String sql = "DELETE FROM produit WHERE id=?";
+        try {
+            pst=connection.prepareStatement(sql);
+            pst.setString(1, String.valueOf(id));
+            System.out.println("Success d'exec requete");
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public Produit getOne(long id) {
         PreparedStatement pst=null;
-        String sql = "SELECT * FROM produit WHERE id Like ?";
+        String sql = "SELECT * FROM produit WHERE id = ?";
         Produit produit=null;
         try {
             pst=connection.prepareStatement(sql);
+            pst.setNString(1,String.valueOf(id));
             System.out.println("Success d'exec requete");
             ResultSet rs=pst.executeQuery();
-            produit=new Produit(rs.getLong("id"),rs.getString("designation"),rs.getInt("qte"),rs.getDouble("prix"),rs.getDate("date").toLocalDate());
+            if(rs.next())
+            return new Produit(rs.getLong("id"),rs.getString("designation"),rs.getInt("qte"),rs.getDouble("prix"),rs.getDate("date").toLocalDate());
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return produit;
+        return null;
     }
 
     @Override
